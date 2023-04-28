34a1af4d3e56e0d6118c13603fe6256a48df534f
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-04-28 21:15:28 +0900

    add a comment explaining the intent of the program

--- lib/eval.ml
+++ lib/eval.ml
@@ -1,5 +1,6 @@
 let apply_prim op args =
   match op with
+  (* NOTE: `args` is ensured not to be an empty list (See `parser.mll`). *)
   | Syntax.Plus -> List.fold_left ( + ) (List.hd args) (List.tl args)
   | Mult -> List.fold_left ( * ) (List.hd args) (List.tl args)
 
